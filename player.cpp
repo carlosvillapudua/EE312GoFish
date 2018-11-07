14f414bf8f24c87e957410dd4f7c7f8b46a93ac0
rithviknimma <36138912+rithviknimma@users.noreply.github.com>
2018-11-06 21:28:32 -0600

    Updated most of player

--- player.cpp
+++ player.cpp
@@ -3,3 +3,90 @@
 //
 
 #include "player.h"
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include <time.h>
+
+using namespace std;
+	
+	Player::Player(){
+		myName = "";
+		unsigned int currentTime = (unsigned) time(0);
+        srand(currentTime);		
+	}
+
+	Player::addCard(Card c){
+		myHand.push_back(c);
+		unsigned int currentTime = (unsigned) time(0);
+        srand(currentTime);		
+	}
+
+	Player::bookCards(Card c1, Card c2){
+		myBook.push_back(c1);
+		myBook.push_back(c2);
+	}
+
+
+	Card Player::chooseCardFromHand() const{
+		int randCard = rand() % myHand.size();
+		return myHand[randCard];
+	}
+
+	bool Player::cardInHand(Card c) const{
+		vector<Card>::iterator iter;
+
+		for(iter = myHand.begin() ; iter != myHand.end() ; iter++){        
+			if(c.getRank() == (*iter).getRank()){
+				return true;
+			}  
+		}
+
+		return false;
+	}
+	//confused about why returning the same card removed
+	Card Player::removeCardFromHand(Card c){
+//int i = 0;
+		Card retCard = c;
+		vector<Card>::iterator iter;
+		iter = myHand.begin();
+
+		while((*iter).getRank() != c.getRank()){
+			iter++;
+//i++
+		}
+
+//myHand.erase(myhand.begin()+i);
+		myHand.erase(iter);
+		return retCard;
+	}
+
+	string showHand() const{
+		vector<Card>::iterator iter;
+		iter = myHand.begin();
+
+		cout << "Cards in " << myName << "'s hand:" << endl;
+		for(iter = myHand.begin() ; iter != myHand.end() ; iter++){
+			cout << *iter << endl;
+		}
+	}
+
+	string showBooks() const{
+		vector<Card>::iterator iter;
+		iter = myBook.begin();
+
+		cout << "Books of :" << myName << endl;
+		for(iter = myBook.begin() ; iter != myBook.end() ; iter = iter + 2){
+			cout << *iter << " " << *(iter + 1) << endl;
+		}
+	}
+
+	int getHandSize() const{
+
+	}
+
+	int getBookSize() const{
+
+	}
